7900f28b096d1a2d149d1da73b2295e67cf0dac9
raiana773 <104357057+raiana773@users.noreply.github.com>
2023-07-11 20:39:24 +0545

    init

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import MainPage from "../pages/MainPage";
 import AuthPage from "../contexts/AuthContext";
+import MainLayout from "../layouts/MainLayout";
 
 function MainRoutes() {
   return (
     <Routes>
-      <Route element={<MainPage />} />
-      <Route path="/" element={<AuthPage />} />
+      <Route element={<MainLayout />} />
+      <Route path="/" element={<MainPage />} />
     </Routes>
   );
 }
